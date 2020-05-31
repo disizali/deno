2d86557164d6df82c761f2b4f19d0e78d5be1ca8
Ali Hasani <a.hassssani@gmail.com>
2020-05-31 15:20:31 +0430

    format

--- cli/main.rs
+++ cli/main.rs
@@ -449,7 +449,6 @@ async fn bundle_command(
   } else {
     println!("{}", output_string);
   }
-
   Ok(())
 }
 
@@ -458,17 +457,17 @@ fn human_size(bytse: f64) -> String {
   let bytse = bytse.abs();
   let units = ["Bytes", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
   if bytse < 1_f64 {
-      return format!("{}{} {}", negative, bytse, "Bytes");
+    return format!("{}{} {}", negative, bytse, "Bytes");
   }
   let delimiter = 1000_f64;
   let exponent = std::cmp::min(
-      (bytse.ln() / delimiter.ln()).floor() as i32,
-      (units.len() - 1) as i32,
+    (bytse.ln() / delimiter.ln()).floor() as i32,
+    (units.len() - 1) as i32,
   );
   let pretty_bytes = format!("{:.2}", bytse / delimiter.powi(exponent))
-      .parse::<f64>()
-      .unwrap()
-      * 1_f64;
+    .parse::<f64>()
+    .unwrap()
+    * 1_f64;
   let unit = units[exponent as usize];
   format!("{}{} {}", negative, pretty_bytes, unit)
 }
